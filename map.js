84e844fb6f4cb50146d42feb8f8ac0eb74c35e29
Surbiton03 <jookyoungbiz@gmail.com>
2026-05-15 22:25:57 -0700

    Add Boston and Cambridge bike lanes

--- map.js
+++ map.js
@@ -1,14 +1,56 @@
+// Import Mapbox as an ESM module
 import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 
+// Check that Mapbox GL JS is loaded
 console.log('Mapbox GL JS Loaded:', mapboxgl);
 
-mapboxgl.accessToken = 'pk.eyJ1Ijoic3VyYml0b24wMyIsImEiOiJjbXA3dnlrc2swODc3MnNweHFncGx2dXZxIn0.lOxlQESKqumf9Ds5_8FoMw';
+// Set your Mapbox access token here
+mapboxgl.accessToken = 'YOUR_ACCESS_TOKEN_HERE';
 
+// Initialize the map
 const map = new mapboxgl.Map({
-  container: 'map',
-  style: 'mapbox://styles/mapbox/streets-v12',
-  center: [-71.09415, 42.36027],
-  zoom: 12,
-  minZoom: 5,
-  maxZoom: 18,
+  container: 'map', // ID of the div where the map will render
+  style: 'mapbox://styles/mapbox/streets-v12', // Map style
+  center: [-71.09415, 42.36027], // [longitude, latitude]
+  zoom: 12, // Initial zoom level
+  minZoom: 5, // Minimum allowed zoom
+  maxZoom: 18, // Maximum allowed zoom
+});
+
+// Define shared line style for bike lanes
+const bikeLaneStyle = {
+  'line-color': '#32D400', // Bright green
+  'line-width': 5,         // Line thickness
+  'line-opacity': 0.6,     // Transparency
+};
+
+// Wait for the map to fully load before adding data
+map.on('load', async () => {
+  // Add Boston bike lanes data source
+  map.addSource('boston_route', {
+    type: 'geojson',
+    data: 'https://bostonopendata-boston.opendata.arcgis.com/datasets/boston::existing-bike-network-2022.geojson',
+  });
+
+  // Add Boston bike lanes layer
+  map.addLayer({
+    id: 'boston-bike-lanes',
+    type: 'line',
+    source: 'boston_route',
+    paint: bikeLaneStyle,
+  });
+
+  // Add Cambridge bike lanes data source
+  map.addSource('cambridge_route', {
+    type: 'geojson',
+    data: 'https://raw.githubusercontent.com/cambridgegis/cambridgegis_data/main/Recreation/Bike_Facilities/RECREATION_BikeFacilities.geojson',
+  });
+
+  // Add Cambridge bike lanes layer
+  map.addLayer({
+    id: 'cambridge-bike-lanes',
+    type: 'line',
+    source: 'cambridge_route',
+    paint: bikeLaneStyle,
+  });
 });
